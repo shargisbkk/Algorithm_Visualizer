5559140e141c76ed5c3f1109ec7b2618a344fc89
Spencer Hargis <shargis664@gmail.com>
2025-12-16 15:42:27 -0500

    imported bubblesort to app, created constants for input, steps for the sort, added a useEffect to create a timer to animate through the steps
    changed return to have an input for users, a button to run the bubblesort, and should provide a barchart

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,52 @@
-import { useState } from 'react'
+import { useState, useEffect, use } from 'react'
+import { bubbleSort } from './algorithms/bubbleSort'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { step } from './algorithms/types'
 
 function App() {
+  const [input, setInput] = useState("") // Input string for the array to be sorted
+  const [steps, setSteps] = useState([]) // Array of sorting steps
+  const [current, setCurrent] = useState(0) // Current step index
   const [count, setCount] = useState(0)
 
+  const run = () => {
+    // Parse input and generate sorting steps
+    const array = input.split(",").map((num) => parseInt(num.trim(), 10))
+    const sortingSteps = bubbleSort(array)
+    setSteps(sortingSteps)
+  };
+
+  //creates a timer that fires every 500ms to update the current step
+  useEffect(() => {
+    // Animate through the steps
+    if (steps.length === 0) return;
+     const id = setInterval(() => {
+        setCurrent(c => (c < steps.length - 1) ? c + 1 : c);
+     }, 500);
+      return () => clearInterval(id);
+    }, [steps]);
+
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{ padding: 20 }}>
+      <h1>Algorithm Visualizer</h1>
+
+      <input
+        value={input}
+        onChange = {e => setInput(e.target.value)}
+        placeholder="Enter numbers separated by commas"
+      />
+      
+      <button onClick={run}>Run Bubble Sort</button>
+
+      {steps.length > 0 && (
+        <>
+          <BarChart step = {steps[current]} />
+          <p>{steps[current].message}</p>
+        </>
+      )}
+    </div>
   )
 }
 
